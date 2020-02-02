2ae8afea34392ea007858157dd8ed2ddaeba7c70
Supanida Supjaroen <59952910+toonjoon@users.noreply.github.com>
2020-02-02 21:28:10 +0700

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -10,5 +10,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);                       // wait for a second
+  delay(1000);                       // wait for a second
 }
