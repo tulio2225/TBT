012dff74ef49c41575e392f3ce41fe2e52053984
Supanida Supjaroen <59952910+toonjoon@users.noreply.github.com>
2020-02-02 21:29:32 +0700

    Update Blink_EXT.ino

--- Blink_EXT.ino
+++ Blink_EXT.ino
@@ -1,25 +1,14 @@
-int LED = D5;    
-int buttonPin = D8;   
-bool buttonState = 0;      
-bool Mode = 0;            
-
-void setup()
-{
-  pinMode(buttonPin, INPUT);    
-  pinMode(LED , OUTPUT);
+#define LED_BUILTIN D4
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-  if (digitalRead(buttonPin)) 
-  {
-    if (!buttonState) 
-    {
-      buttonState = true;
-      Mode = !Mode; 
-    }
-  }
-  else buttonState = false;
-  digitalWrite(LED , Mode); 
-  delay(5);
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
 }
