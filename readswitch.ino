7a75edb06383b3050bb9e0ea8a4ba394eda4084c
Supanida Supjaroen <59952910+toonjoon@users.noreply.github.com>
2020-02-02 20:50:38 +0700

    Update readswitch.ino

--- readswitch.ino
+++ readswitch.ino
@@ -1,13 +1,21 @@
-
+#define LED_BUILTIN D4
+// the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5,INPUT);
+  //Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   
-  delay(1000);                       
-  digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);                       
+  bool sw = digitalRead(5);
+  //Serial.println(sw);
+  if(sw == 1){
+    digitalWrite(LED_BUILTIN,1);
+  }
+  else{
+    digitalWrite(LED_BUILTIN,0);
+  }
 }
+
